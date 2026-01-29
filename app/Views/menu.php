420b6764e2199de7059853250f52e0937cf6f4aa
Lucas Vianna <vianna_lsv@hotmail.com>
2026-01-29 01:04:32 -0300

    Adicionado Item Cobrança no menu

--- app/Views/menu.php
+++ app/Views/menu.php
@@ -11,6 +11,29 @@
         <ul class="side-menu pt-3" id="menuPermissoes">
             <li class="slide d-none">
                 <a class="side-menu__item" data-toggle="slide" href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="side-menu__icon bi bi-cash-coin" viewBox="0 0 20 20">
+                        <path fill-rule="evenodd" d="M11 15a4 4 0 1 0 0-8 4 4 0 0 0 0 8m5-4a5 5 0 1 1-10 0 5 5 0 0 1 10 0"/>
+                        <path d="M9.438 11.944c.047.596.518 1.06 1.363 1.116v.44h.375v-.443c.875-.061 1.386-.529 1.386-1.207 0-.618-.39-.936-1.09-1.1l-.296-.07v-1.2c.376.043.614.248.671.532h.658c-.047-.575-.54-1.024-1.329-1.073V8.5h-.375v.45c-.747.073-1.255.522-1.255 1.158 0 .562.378.92 1.007 1.066l.248.061v1.272c-.384-.058-.639-.27-.696-.563h-.668zm1.36-1.354c-.369-.085-.569-.26-.569-.522 0-.294.216-.514.572-.578v1.1zm.432.746c.449.104.655.272.655.569 0 .339-.257.571-.709.614v-1.195z"/>
+                        <path d="M1 0a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h4.083q.088-.517.258-1H3a2 2 0 0 0-2-2V3a2 2 0 0 0 2-2h10a2 2 0 0 0 2 2v3.528c.38.34.717.728 1 1.154V1a1 1 0 0 0-1-1z"/>
+                        <path d="M9.998 5.083 10 5a2 2 0 1 0-3.132 1.65 6 6 0 0 1 3.13-1.567"/>
+                    </svg>
+                    <span class="side-menu__label">Cobranças</span>
+                    <i class="angle fe fe-chevron-down"></i>
+                </a>
+                <ul class="slide-menu">
+                    <?PHP if (\App\Models\UsuarioModel::getSessao()->verificarPermissao('Cobranca', 'cadastrar')) { ?>
+                        <li><a class="slide-item" href="<?PHP echo base_url('Cobranca/cadastrar'); ?>">Cadastrar</a></li>
+                    <?PHP } ?>
+                    <?PHP if (\App\Models\UsuarioModel::getSessao()->verificarPermissao('Cobranca', 'pesquisar')) { ?>
+                        <li><a class="slide-item" href="<?PHP echo base_url('Cobranca/pesquisar'); ?>">Pesquisar</a></li>
+                    <?PHP } ?>
+                    <?PHP if (\App\Models\UsuarioModel::getSessao()->verificarPermissao('Cobranca', 'listar')) { ?>
+                        <li><a class="slide-item" href="<?PHP echo base_url('Cobranca/listar'); ?>">Listar</a></li>
+                    <?PHP } ?>
+                </ul>
+            </li>
+            <li class="slide d-none">
+                <a class="side-menu__item" data-toggle="slide" href="#">
                     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="side-menu__icon bi bi-calendar2-heart" viewBox="0 0 20 20">
                         <path fill-rule="evenodd" d="M4 .5a.5.5 0 0 0-1 0V1H2a2 2 0 0 0-2 2v11a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2h-1V.5a.5.5 0 0 0-1 0V1H4zM1 3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v11a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1zm2 .5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h10a.5.5 0 0 0 .5-.5V4a.5.5 0 0 0-.5-.5zm5 4.493c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132" />
                     </svg>
